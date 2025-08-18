a014c7e7e674ff94705b317c2707eb3a6f05c23f
Mehmet Özerli <memolim1765@gmail.com>
2025-08-18 12:03:38 +0300

    drawer yapısı oluşturuldu

--- js/components.js
+++ js/components.js
@@ -5,3 +5,35 @@ export async function loadNavbar() {
     document.querySelector("header").innerHTML=html;
 }
 
+document.addEventListener("DOMContentLoaded" , () => {
+    const drawer = document.getElementById("drawer");
+    const overlay= document.getElementById("overlay");
+    const drawerBtn=document.getElementById("drawerBtn");
+    const closeDrawer=document.getElementById("closeDrawer");
+
+    if (drawerBtn) {
+        drawerBtn.addEventListener("click", () =>  {
+            drawer.classList.add("open");
+            overlay.classList.add("show");
+        });
+
+    }
+
+    if (closeDrawer) {
+        closeDrawer.addEventListener("click", () => {
+            drawer.classList.remove("open");
+            overlay.classList.remove("show");
+
+        });
+    }
+    
+    if (overlay) {
+        overlay.addEventListener("click",() => {
+            drawer.classList.remove("open");
+            overlay.classList.remove("show");
+        });
+    }
+
+}); 
+
+    
